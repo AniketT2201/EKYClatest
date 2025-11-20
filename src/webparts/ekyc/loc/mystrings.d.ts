25bb8e7f66b25b8f7de02fb896609e3801f38da6
AniketT2201 <Aniket@isritechnologies.com>
2025-11-20 11:03:06 +0530

    .

---
+++ src/webparts/ekyc/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IEkycWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'EkycWebPartStrings' {
+  const strings: IEkycWebPartStrings;
+  export = strings;
+}
---
+++ src/webparts/ekyc/services/BAL/SPCRUD/spcrud.ts
@@ -0,0 +1,92 @@
+import "@pnp/sp/lists";
+import "@pnp/sp/items";
+// import { IPatelEngProps } from "../../components/IPatelEngProps";
+import { IEkycProps } from "../../../components/IEkycProps";
+import SPCRUDOPS from "../../DAL/spcrudops";
+
+export interface ISPCRUD {
+    getData(listName: string, columnsToRetrieve: string, columnsToExpand: string, filters: string, orderby: { column: string, isAscending: boolean }, top: number, props: IEkycProps): Promise<any>;
+    getRootData(listName: string, columnsToRetrieve: string, columnsToExpand: string, filters: string, orderby: { column: string, isAscending: boolean }, top: number, props: IEkycProps): Promise<any>;
+    insertData(listName: string, data: any, props: IEkycProps): Promise<any>;
+    updateData(listName: string, itemId: number, data: any, props: IEkycProps): Promise<any>;
+    deleteData(listName: string, itemId: number, props: IEkycProps): Promise<any>;
+    getListInfo(listName: string, props: IEkycProps): Promise<any>;
+    getListData(listName: string, columnsToRetrieve: string, props: IEkycProps): Promise<any>;
+    batchInsert(listName: string, data: any, props: IEkycProps): Promise<any>;
+    batchUpdate(listName: string, data: any, props: IEkycProps): Promise<any>;
+    batchDelete(listName: string, data: any, props: IEkycProps): Promise<any>;
+    createFolder(listName: string, folderName: string, props: IEkycProps): Promise<any>;
+    uploadFile(folderServerRelativeUrl: string, file: File, props: IEkycProps): Promise<any>;
+    deleteFile(fileServerRelativeUrl: string, props: IEkycProps): Promise<any>;
+    currentProfile(props: IEkycProps): Promise<any>;
+    getLoggedInSiteGroups(props: IEkycProps): Promise<any>;
+    getAllSiteGroups(props: IEkycProps): Promise<any>;
+    getTopData(listName: string, columnsToRetrieve: string, columnsToExpand: string, filters: string
+        , orderby: { column: string, isAscending: boolean }, top: number, props: IEkycProps): Promise<any>;
+    addAttchmentInList(attFiles: File, listName: string, itemId: number, fileName: string, props: IEkycProps): Promise<any>;
+
+}
+
+export default async function USESPCRUD(): Promise<ISPCRUD> {
+    const spCrudOps = await SPCRUDOPS();
+    return {
+        getData: async (listName: string, columnsToRetrieve: string, columnsToExpand: string, filters: string
+            , orderby: { column: string, isAscending: boolean }, top: number, props: IEkycProps) => {
+            return await spCrudOps.getData(listName, columnsToRetrieve, columnsToExpand, filters, orderby, props);
+        },
+        getRootData: async (listName: string, columnsToRetrieve: string, columnsToExpand: string, filters: string
+            , orderby: { column: string, isAscending: boolean }, top: number, props: IEkycProps) => {
+            return await spCrudOps.getData(listName, columnsToRetrieve, columnsToExpand, filters, orderby, props);
+        },
+        insertData: async (listName: string, data: any, props: IEkycProps) => {
+            return await spCrudOps.insertData(listName, data, props);
+        },
+        updateData: async (listName: string, itemId: number, data: any, props: IEkycProps) => {
+            return await spCrudOps.updateData(listName, itemId, data, props);
+        },
+        deleteData: async (listName: string, itemId: number, props: IEkycProps) => {
+            return await spCrudOps.deleteData(listName, itemId, props);
+        },
+        getListInfo: async (listName: string, props: IEkycProps) => {
+            return await spCrudOps.getListInfo(listName, props);
+        },
+        getListData: async (listName: string, columnsToRetrieve: string, props: IEkycProps) => {
+            return await spCrudOps.getListData(listName, columnsToRetrieve, props);
+        },
+        batchInsert: async (listName: string, data: any, props: IEkycProps) => {
+            return await spCrudOps.batchInsert(listName, data, props);
+        },
+        batchUpdate: async (listName: string, data: any, props: IEkycProps) => {
+            return await spCrudOps.batchUpdate(listName, data, props);
+        },
+        batchDelete: async (listName: string, data: any, props: IEkycProps) => {
+            return await spCrudOps.batchDelete(listName, data, props);
+        },
+        createFolder: async (listName: string, folderName: string, props: IEkycProps) => {
+            return await spCrudOps.createFolder(listName, folderName, props);
+        },
+        uploadFile: async (folderServerRelativeUrl: string, file: File, props: IEkycProps) => {
+            return await spCrudOps.uploadFile(folderServerRelativeUrl, file, props);
+        },
+        deleteFile: async (fileServerRelativeUrl: string, props: IEkycProps) => {
+            return await spCrudOps.deleteFile(fileServerRelativeUrl, props);
+        },
+        currentProfile: async (props: IEkycProps) => {
+            return await spCrudOps.currentProfile(props);
+        },
+
+        getLoggedInSiteGroups: async (props: IEkycProps) => {
+            return await spCrudOps.getLoggedInSiteGroups(props);
+        },
+        getAllSiteGroups: async (props: IEkycProps) => {
+            return await spCrudOps.getAllSiteGroups(props);
+        },
+        getTopData: async (listName: string, columnsToRetrieve: string, columnsToExpand: string, filters: string
+            , orderby: { column: string, isAscending: boolean }, top: number, props: IEkycProps) => {
+            return await spCrudOps.getTopData(listName, columnsToRetrieve, columnsToExpand, filters, orderby, top, props);
+        },
+        addAttchmentInList: async (attFiles: File, listName: string, itemId: number, fileName: string, props: IEkycProps) => {
+            return await spCrudOps.addAttchmentInList(attFiles, listName, itemId, fileName, props);
+        }
+    };
+}
